aefb3100046eaf208af8fc62d77ac78317470074
kishore kumar <kishore.behera2010@gmail.com>
2022-05-11 20:41:52 +0530

    learning from codewithmosh.com

---
+++ CodeWMosh/app.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("*" * 10)
+x = 1
+students_count = 1000
+print(students_count)
+course = "Python Programming"
+print(len(course))
+print(course[1])
+print(course[-1])
+print(course.upper())
+# x = int(input("x: "))
+# y = x+1
+# print(y)
+for x in range(5):
+    for y in range(3):
+        print(f"({x},{y})")
+
+for i in range(1, 10):
+    if i % 2 == 0:
+        print(i)
+print("We have 4 even numbers")
