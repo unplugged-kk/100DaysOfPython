c58420bb6abc07bacc9afae7c76797449ccfecd5
Kishore Kumar Behera <kishore.behera2010@gmail.com>
2022-06-01 23:42:54 +0530

    Completed boto3 waiters

--- AWS_BOTO3_PYTHON/Udemy_Learning/working_w_ec2_waiters_using_clients.py
+++ AWS_BOTO3_PYTHON/Udemy_Learning/working_w_ec2_waiters_using_clients.py
@@ -1,5 +1,4 @@
 import boto3
-import time
 
 aws_mgmt_console = boto3.session.Session(profile_name="syler")
 ec2_con_cli = aws_mgmt_console.client(
@@ -8,9 +7,7 @@ ec2_con_re = aws_mgmt_console.resource(
     service_name="ec2", region_name="us-east-1")
 
 instance_id = input("Please enter EC2 instance Id: ")
-instance_ob = ec2_con_re.Instance(instance_id)
-print(f"Starting {instance_id} ......")
-instance_ob.start()
+
 
 # The while block is called as waiter , in stead of using this long logic we can use boto3 waiter
 
@@ -23,9 +20,10 @@ instance_ob.start()
 #     print("Waiting for EC2 server to come up")
 #     time.sleep(5)
 
-# boto3 waiter !!
-
-# Resouce waits for 200 secs (40 checks after 5 sec)
-instance_ob.wait_until_running()
+# boto3 waiter  using client !!
+print(f"Starting {instance_id} EC2 instance ")
+ec2_con_cli.start_instances(InstanceIds=[instance_id])
+waiter = ec2_con_cli.get_waiter('instance_running')
+waiter.wait(InstanceIds=[instance_id])
 
 print(f"The EC2 instance {instance_id} is Up & running now !!")
