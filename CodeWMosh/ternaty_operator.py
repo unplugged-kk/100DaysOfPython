5cd92da81774faed63183687d352437a187e9ed4
kishore kumar <kishore.behera2010@gmail.com>
2022-12-27 23:17:04 +0530

    CodeWMosh Revision

---
+++ CodeWMosh/ternaty_operator.py
@@ -0,0 +1,11 @@
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("You are eligible")
+# else:
+#     print("You are not eligible")
+
+
+age = int(input("Enter your age: "))
+
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
